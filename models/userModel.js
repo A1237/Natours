32829dbe7fb095cc4d23b671831b3df933117af7
Akash <akashorasad2000@gmail.com>
2019-10-18 01:27:08 +0530

    commit

--- models/userModel.js
+++ models/userModel.js
@@ -79,7 +79,7 @@ userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
 userSchema.methods.createPasswordResetToken = function () {
     const resetToken = crypto.randomBytes(32).toString('hex');
 
-    this.passwordResetToken = crypto.createHash('sha258').update(resetToken).digest('hex')
+    this.passwordResetToken = crypto.createHash('sha256').update(resetToken).digest('hex')
 
     console.log({
         resetToken
